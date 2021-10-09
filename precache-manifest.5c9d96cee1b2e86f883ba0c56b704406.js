9e8186a03b68cc2fc5f865f21c36690ad4a38304
YihuiTham <yihuitham@gmail.com>
2021-10-09 09:34:21 +0800

    Updates

---
+++ precache-manifest.5c9d96cee1b2e86f883ba0c56b704406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1924f15f8453fbc4820c88dda21c036",
+    "url": "/react-weather-icons/index.html"
+  },
+  {
+    "revision": "9aa4ecee1f4ecfaf7882",
+    "url": "/react-weather-icons/static/css/main.f35f9068.chunk.css"
+  },
+  {
+    "revision": "8c9381baee04e88dd385",
+    "url": "/react-weather-icons/static/js/2.f752d733.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-icons/static/js/2.f752d733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa4ecee1f4ecfaf7882",
+    "url": "/react-weather-icons/static/js/main.2b24765a.chunk.js"
+  },
+  {
+    "revision": "be88c940c2326fd303a5",
+    "url": "/react-weather-icons/static/js/runtime-main.f6a6f39e.js"
+  }
+]);
